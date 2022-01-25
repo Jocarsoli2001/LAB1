d3b55cbfd3e38dc8280f6f97aad4740fb2ab82d0
Jocarsoli2001 <Jocarsoli2001@gmail.com>
2022-01-24 20:59:07 -0600

    Laboratorio 1 - Creación de Libreria
    Se creo el archivo de la libreria para realizar el ADC y mostrar valores en displays de 7 segmentos

---
+++ PROYECTO MPLAB/ADC-7seg.h
@@ -0,0 +1,17 @@
+/* 
+ * File: ADC-7seg.h   
+ * Author: José Santizo (20185)
+ * Comments: Libreria que incluye la configuración de ADC y la traducción para display de 7 segmentos
+ * Revision history: Creado el 24 de enero de 2022
+ */
+
+#ifndef ADC7seg_H
+#define	ADC7seg_H
+
+#include <xc.h>                         // include processor files - each processor file is guarded.  
+
+//-------------------------Función para obtener valor de ADC-----------------------
+
+
+#endif	/* ADC7seg_H */
+
